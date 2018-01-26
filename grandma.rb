229fd43354e09c1712634e70b8fc78b5a7f18444
Harleigh Abel <harleighabel@gmail.com>
2018-01-26 20:42:47 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,6 @@
-def speak_to_grandma(you_say)
-  if you_say == "I LOVE YOU GRANDMA!"
+def speak_to_grandma(phrase)
+  if phrase == "I LOVE YOU GRANDMA!"
       puts " I LOVE YOU TOO PUMPKIN!"
   end
 end
+
